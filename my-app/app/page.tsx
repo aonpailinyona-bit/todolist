1d1f24d7dbda024b2ff6dc8682f2177eafdb4cc2
ชื่อของคุณ <@email.com>
2026-08-01 16:03:59 +0700

    feat: force redirect to signin via page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,53 +1,5 @@
-'use client';
-import React from 'react';
-import { useState, useEffect, useRef } from 'react';
-import Image from 'next/image';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const [books, setBooks] = useState([
-  { id: 100, name: 'java' },
-  { id: 200, name: 'php' },
-  { id: 300, name: 'C#' },
-  { id: 400, name: 'python' }
-])
-  const [massage, setMassage] = React.useState('Hello');
-  const [change, setChange] = useState('')
-  const isFirst = useRef(true);
-  const textBox = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    textBox.current?.focus();
-  }, [])
-
-  useEffect (() => {
-    if (isFirst.current) {
-      isFirst.current = false;
-      return;
-    }
-  
-
-  console.log('massage = ', massage)
-  setChange('massage change massage is ' + massage)
-  }, [massage])
-
-  useEffect(() => {
-    if (massage !== '') {
-      setChange('massage change massage is ' + massage)
-    }
-  }, [massage])
-  return (
-    <div>
-      <div className="p-4">
-        <div className="mt-10 mb-10">
-          <img alt='my picture' src='/next.svg' width={200} height={200} />
-        </div>
-        <button className='button'>My Button</button>
-        {books.map(item => (
-        <div key={item.id}>
-          {item.id} : {item.name}
-        </div>
-      ))}
-      </div>
-    </div>
-  );
+  redirect('/backoffice/signin');
 }
